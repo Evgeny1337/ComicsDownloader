a1033fc7d539086f775d6b492bfd8f9da89ec14d
Evgeny1337 <genya-sozykin@mail.ru>
2025-03-20 13:40:04 +0300

    edit image downloader

--- downloader.py
+++ downloader.py
@@ -1,6 +1,6 @@
 import requests
 import os
-
+import argparse
 
 def get_total_comics():
     response = requests.get('https://xkcd.com/info.0.json')
@@ -13,17 +13,39 @@ def get_comic(num):
     response = requests.get(img_url)
     response.raise_for_status()
     comic_data = response.json()
-    # img_link = comic_data['img']
-    # img_response = requests.get(img_link)
-    # img_response.raise_for_status()
     return comic_data
 
 
-def download_image(img_response,num):
+def download_image(comic_data,num):
+    img_link = comic_data['img']
+    img_response = requests.get(img_link)
+    img_response.raise_for_status()
     img_name = os.path.join('images', f'{num}.png')
+    img_alt = comic_data['alt']
     with open(img_name, 'wb') as img_file:
         img_file.write(img_response.content)
     print(f'Изображение {num} сохранено в {img_name}.')
+    return img_alt
 
-def download_about(img_response,num):
-    return "{} - {}".format(num,img_response['alt'])
+def load_comics(max_load=50):
+    images_count = get_total_comics()
+    for num in range(1, images_count):
+        comic = get_comic(num)
+        download_image(comic_data=comic, num=num)
+        if num == max_load:
+            break
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='Загрузчик ищображений',
+        description='Загружает изображения из комиксов',
+    )
+    parser.add_argument('-c', '--count', help='Максимальное количество выгруженных изображений', default=5, type=int)
+    args = parser.parse_args()
+    load_comics(args.count)
+
+
+if __name__ == "__main__":
+    main()
+
+
