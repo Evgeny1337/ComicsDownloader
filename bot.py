d2fc1b7b62960d1c5bce61b35b153d706c15d24a
Evgeny1337 <genya-sozykin@mail.ru>
2025-04-04 12:26:36 +0300

    changed the library and added the function of sending comics to telegram

--- bot.py
+++ bot.py
@@ -1,23 +1,37 @@
-import asyncio
-import logging
-import sys
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, html
-from os import environ
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram import F
-from aiogram.filters import CommandStart
-
-load_dotenv()
-TOKEN = environ['BOT_TOKEN']
-bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+from os import environ, path
+import random
+import logging
+from bot_helper import get_comic, download_image
+import os
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    )
 
 
-async def main():
-    dp = Dispatcher()
+async def get_picture(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    max_photo_size = 20 * 1024 * 1024
+    total_comics = get_comic('https://xkcd.com/info.0.json')['num']
+    random_number = random.randrange(0, total_comics)
+    comic_data = get_comic(f'https://xkcd.com/{random_number}/info.0.json')
+    send_data = download_image(comic_data)
+    img_name = send_data['img_name']
+    img_description = send_data['img_alt']
+    if path.isfile(img_name) and path.getsize(img_name) > 0 and path.getsize(img_name) <= max_photo_size:
+        with open(img_name, 'rb') as image:
+            await update.message.reply_photo(photo=image, caption=img_description, filename=img_name)
+        os.remove(img_name)
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+def main():
+    load_dotenv()
+    tg_token = environ['TELEGRAM_TOKEN']
+    application = Application.builder().token(tg_token).build()
+    application.add_handler(CommandHandler('get', get_picture))
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == '__main__':
+    main()
---
+++ bot_helper.py
@@ -0,0 +1,23 @@
+import requests
+import os
+
+def get_comic(img_url):
+    response = requests.get(img_url)
+    response.raise_for_status()
+    comic_data = response.json()
+    return comic_data
+
+
+def download_image(comic_data):
+    img_link = comic_data['img']
+    img_response = requests.get(img_link)
+    img_response.raise_for_status()
+    img_number = comic_data['num']
+    img_name = os.path.join('images', f'{img_number}.png')
+    img_alt = comic_data['alt']
+    title = comic_data['title']
+    with open(img_name, 'wb') as img_file:
+        img_file.write(img_response.content)
+    print(f'Изображение {img_number} сохранено в {img_name}.')
+    return {"img_alt":img_alt,"title":title,"img_name":img_name}
+
