f4d41564e85fb2c10d3202f5e611d709014b747b
Evgeny1337 <genya-sozykin@mal.ru>
2025-06-25 11:08:07 +0500

    fixed name of functions, added logger

--- bot.py
+++ bot.py
@@ -3,36 +3,52 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 from os import environ, path
 import random
-import logging
 from bot_helper import get_comic, download_image
 import os
+from tempfile import NamedTemporaryFile
+import logging
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-    )
+logger = logging.getLogger(__file__)
 
-
-async def get_picture(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    max_photo_size = 20 * 1024 * 1024
+async def get_random_comic():
     total_comics = get_comic('https://xkcd.com/info.0.json')['num']
     random_number = random.randrange(0, total_comics)
     comic_data = get_comic(f'https://xkcd.com/{random_number}/info.0.json')
-    send_data = download_image(comic_data)
-    img_name = send_data['img_name']
-    img_description = send_data['img_alt']
-    if path.isfile(img_name) and path.getsize(img_name) > 0 and path.getsize(img_name) <= max_photo_size:
-        with open(img_name, 'rb') as image:
-            await update.message.reply_photo(photo=image, caption=img_description, filename=img_name)
-        os.remove(img_name)
+    image_data = download_image(comic_data)
+    return image_data
 
+async def send_comic(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    max_photo_size = 20 * 1024 * 1024
+    image_data = await get_random_comic()
+    img_name = image_data['img_name']
+    img_content = image_data['img_content']
+    img_description = image_data['img_alt']
+    img_size = len(img_content)
+    if img_size == 0 or img_size > max_photo_size:
+        await update.message.reply_text(f"Недопустимый размер изображения: {img_size} байт")
+        return
+    
+    with NamedTemporaryFile(suffix=".png", delete=True) as tmp_file:
+        tmp_file.write(img_content)
+        tmp_file.flush()
+        tmp_file.seek(0)
+
+        await update.message.reply_photo(
+            photo=tmp_file,
+            caption=img_description,
+            filename=os.path.basename(img_name)
+        )
+    
 
 def main():
     load_dotenv()
-    tg_token = environ['TELEGRAM_TOKEN']
+    tg_token = environ['TG_TOKEN']
     application = Application.builder().token(tg_token).build()
-    application.add_handler(CommandHandler('get', get_picture))
+    application.add_handler(CommandHandler('get', send_comic))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.ERROR)
+    logger.setLevel(logging.DEBUG)
     main()
--- bot_helper.py
+++ bot_helper.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from tempfile import NamedTemporaryFile
 
 def get_comic(img_url):
     response = requests.get(img_url)
@@ -12,13 +13,12 @@ def download_image(comic_data):
     img_link = comic_data['img']
     img_response = requests.get(img_link)
     img_response.raise_for_status()
+
     img_number = comic_data['num']
-    img_name = os.path.join('images', f'{img_number}.png')
     img_alt = comic_data['alt']
     title = comic_data['title']
-    os.makedirs('images', exist_ok=True)
-    with open(img_name, 'wb') as img_file:
-        img_file.write(img_response.content)
-    print(f'Изображение {img_number} сохранено в {img_name}.')
-    return {"img_alt": img_alt, "title": title, "img_name": img_name}
+
+    img_name = os.path.join('images', f'{img_number}.png')
+  
+    return {"img_alt": img_alt, "title": title, "img_name": img_name, "img_content":img_response.content}
 
